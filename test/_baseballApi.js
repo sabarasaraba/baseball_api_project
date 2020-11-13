414efc6c8f1bff42e710b4072697f2578adc6fdd
Naoto Kagimoto <kagimotonaoto@gmail.com>
2020-11-13 11:42:31 +0900

    [add] first test ファミリーネームを指定して該当する選手データを返す(Status:200)

---
+++ test/_baseballApi.js
@@ -0,0 +1,27 @@
+const chai = require("chai");
+const { expect } = require("chai");
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+const { setupServer } = require("../src/server");
+const _ = require("lodash");
+
+describe("Baseball API Server", () =>{
+    let request;
+    const server = setupServer();
+    beforeEach(() => {
+        request = chai.request(server);
+    });
+
+    describe("/api/baseball/player/:familyName", () =>{
+        it("ファミリーネームを指定して該当する選手データを返す(Status:200)",function(done) {
+            const expected = 200;
+            request.get("/api/baseball/player/Ohtani").end(function(err, res) {
+                if (err) console.error(err);
+                expect(res).to.have.status(expected);
+                done();
+            });
+        });
+    });
+});
+
+
